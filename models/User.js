15da864a015e295cb6ee61244ca52c49fc8b4e97
i2414977 <i2414977@gmail.com>
2025-11-27 22:52:09 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -17,31 +17,19 @@ const userSchema = new mongoose.Schema({
   documentoIdentita: { type: String, required: true },
   numeroDocumento: { type: String, required: true },
   sottoscritto: { type: String, required: true },
-  autorizzazioneFoto: { 
-    type: String, 
-    enum: ['autorizza', 'non_autorizza', 'non_specificato'],
-    default: 'non_specificato'
-  },
+  dataRichiesta: { type: Date, default: Date.now },
+  tipoSocio: { type: String, default: 'ordinario' },
+  stato: { type: String, default: 'in_attesa' },
+  dataApprovazione: Date,
+  numeroTessera: { type: Number, unique: true, sparse: true },
+  dataEmissioneTessera: Date,
+  statoTessera: { type: String, enum: ['da_emettere', 'emessa', 'inviata', 'scaduta'], default: 'da_emettere'},
+  autorizzazioneFoto: { type: String, enum: ['autorizza', 'non_autorizza', 'non_specificato'], default: 'non_specificato'},
   impegnoNoUsoCommerciale: { type: Boolean, default: false },
   accettaSottoposizione: { type: Boolean, default: false },
   accettaDirittiDoveri: { type: Boolean, default: false },
   consensoTrattamentoDati: { type: Boolean, required: true },
-  numeroTessera: { 
-    type: Number,
-    unique: true,
-    sparse: true
-  },
-  dataApprovazione: Date,
-  dataEmissioneTessera: Date,
-  statoTessera: {
-    type: String,
-    enum: ['da_emettere', 'emessa', 'inviata', 'scaduta'],
-    default: 'da_emettere'
-  },
-  dataRichiesta: { type: Date, default: Date.now },
-  stato: { type: String, default: 'in_attesa' },
-  tipoSocio: { type: String, default: 'ordinario' }
-}, {
+  }, {
   timestamps: true
 });
 
